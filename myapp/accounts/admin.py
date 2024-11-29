c7ac304686c1c87a520f43a8c1c9cb0672aec05b
deepi-del <d4deepkk@gmail.com>
2024-11-29 17:38:29 +0530

    final changes

--- myapp/accounts/admin.py
+++ myapp/accounts/admin.py
@@ -4,13 +4,13 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
 class CustomUserAdmin(BaseUserAdmin):
-    # Specify which fields to display in the list view
+    #  display in the list view
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff')
     list_filter = ('is_active', 'is_staff')
     search_fields = ('username', 'email', 'first_name', 'last_name')
     ordering = ('username',)
     
-    # Fields to use for creating and editing users
+    #  creating  editing users
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
@@ -25,12 +25,12 @@ class CustomUserAdmin(BaseUserAdmin):
         ),
     )
 
-# Register CustomUser with the customized admin class
+
 admin.site.register(CustomUser, CustomUserAdmin)
 
 class AdminProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'department', 'is_superuser')
     search_fields = ('user__username', 'department')
 
-# Register AdminProfile with its own admin class
+
 admin.site.register(AdminProfile, AdminProfileAdmin)
--- myapp/accounts/forms.py
+++ myapp/accounts/forms.py
@@ -1,4 +1,3 @@
-# accounts/forms.py
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
@@ -7,3 +6,16 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'email', 'first_name')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Make the email field read-only if an instance exists (editing mode)
+        if self.instance and self.instance.pk:
+            self.fields['email'].disabled = True
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        # Skip uniqueness validation if the email field is read-only
+        if not self.fields['email'].disabled and CustomUser.objects.filter(email=email).exists():
+            raise forms.ValidationError("A user with this email already exists.")
+        return email
